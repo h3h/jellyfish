2888a879e0fd44681332d802af36528724a44a9b
Brad Fults <bfults@gmail.com>
2010-01-09 09:03:29 -0800

    Capture params from regex groups in bloom matchers.

--- test/test_jellyfish_application.js
+++ test/test_jellyfish_application.js
@@ -2,6 +2,8 @@ var MY_CLICKED_EVENT = 0;
 var MY_EXACT_EXECUTED_BLOOM = 0;
 var MY_OTHER_EXECUTED_BLOOM = 0;
 var MY_REGEX_BLOOM = 0;
+var MY_REGEX_PARAM_0;
+var MY_REGEX_PARAM_1;
 var MY_STRING_FORMAT_BLOOM = 0;
 var MY_WRONG_STRING_FORMAT_BLOOM = 0;
 
@@ -20,6 +22,11 @@ var JellyfishApp = Jellyfish(function () {
     this.sting("#test_link/click", function () {
       MY_CLICKED_EVENT = 1;
     });
+  });
+
+  this.bloom(/\/foo\/([^\/]+)\/([^\/]+)/, function (params) {
+    MY_REGEX_PARAM_0 = params[0];
+    MY_REGEX_PARAM_1 = params[1];
   });
 
   this.bloom('/foo/bar/13', function (params) {
@@ -48,6 +55,10 @@ should('not execute a bloom with a non-matching string', function () {
 }).
 should('execute the correct bloom for a regex matcher', function () {
   equals(MY_REGEX_BLOOM, 1);
+}).
+should('give correct params for regex group matches', function () {
+  equals(MY_REGEX_PARAM_0, 'bar');
+  equals(MY_REGEX_PARAM_1, '13');
 }).
 should('execute the correct bloom for a matching string format', function () {
   equals(MY_STRING_FORMAT_BLOOM, 1);
